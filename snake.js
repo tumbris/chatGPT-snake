1aec74d4e3d6f432553dd9a272659610476a326b
tumbris <sergsy.gerashenko@gmail.com>
2023-03-31 01:48:16 +0300

    another version

--- snake.js
+++ snake.js
@@ -1,193 +1,212 @@
-// Set up the canvas and context
+// Get the canvas and context
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-// Set the block size and number of blocks
-var blockSize = 10;
-var widthInBlocks = canvas.width / blockSize;
-var heightInBlocks = canvas.height / blockSize;
+// Set the canvas dimensions and border width
+canvas.width = 400;
+canvas.height = 400;
+var borderWidth = 20;
 
-// Define the Block constructor
-function Block(col, row) {
-	this.col = col;
-	this.row = row;
-}
-
-// Draw a square on the canvas
-Block.prototype.drawSquare = function(color) {
-	var x = this.col * blockSize;
-	var y = this.row * blockSize;
-	ctx.fillStyle = color;
-	ctx.fillRect(x, y, blockSize, blockSize);
-};
-
-// Draw a circle on the canvas
-Block.prototype.drawCircle = function(color) {
-	var centerX = this.col * blockSize + blockSize / 2;
-	var centerY = this.row * blockSize + blockSize / 2;
-	ctx.fillStyle = color;
-	circle(centerX, centerY, blockSize / 2, true);
-};
-
-// Check if two blocks are equal
-Block.prototype.equal = function(otherBlock) {
-	return this.col === otherBlock.col && this.row === otherBlock.row;
-};
-
-// Define the Snake constructor
+// Define the Snake object
 function Snake() {
-	this.segments = [new Block(7, 5), new Block(6, 5), new Block(5, 5)];
-	this.direction = "right";
-	this.nextDirection = "right";
+  this.size = 20;
+  this.x = canvas.width / 2;
+  this.y = canvas.height / 2;
+  this.direction = "none";
+  this.nextDirection = "none";
+  this.segments = [{ x: this.x, y: this.y }];
+
+  // Define the move method
+  this.move = function () {
+    this.direction = this.nextDirection;
+
+    // Move the head of the snake
+    switch (this.direction) {
+      case "up":
+        this.y -= this.size;
+        break;
+      case "down":
+        this.y += this.size;
+        break;
+      case "left":
+        this.x -= this.size;
+        break;
+      case "right":
+        this.x += this.size;
+        break;
+      default:
+        break;
+    }
+
+    // Add the new head segment to the segments array
+    this.segments.unshift({ x: this.x, y: this.y });
+
+    // Remove the tail segment if the snake hasn't eaten an apple
+    if (this.segments.length > 1 && !this.ateApple) {
+      this.segments.pop();
+    } else {
+      this.ateApple = false;
+    }
+  };
+
+  // Define the draw method
+  this.draw = function () {
+    for (var i = 0; i < this.segments.length; i++) {
+      ctx.fillStyle = "#000";
+      ctx.fillRect(
+        this.segments[i].x,
+        this.segments[i].y,
+        this.size,
+        this.size
+      );
+    }
+  };
+
+  // Define the checkCollision method
+  this.checkCollision = function () {
+    // Check for collision with the border
+    if (
+      this.x < borderWidth ||
+      this.x + this.size > canvas.width - borderWidth ||
+      this.y < borderWidth ||
+      this.y + this.size > canvas.height - borderWidth
+    ) {
+      return true;
+    }
+
+    // Check for collision with the snake's own body
+    for (var i = 1; i < this.segments.length; i++) {
+      if (this.x === this.segments[i].x && this.y === this.segments[i].y) {
+        return true;
+      }
+    }
+
+    return false;
+  };
 }
 
-// Draw the snake on the canvas
-Snake.prototype.draw = function() {
-	for (var i = 0; i < this.segments.length; i++) {
-		this.segments[i].drawSquare("blue");
-	}
-};
-
-// Move the snake on the canvas
-Snake.prototype.move = function() {
-	var head = this.segments[0];
-	var newHead;
-
-	this.direction = this.nextDirection;
-
-	if (this.direction === "right") {
-		newHead = new Block(head.col + 1, head.row);
-	} else if (this.direction === "down") {
-		newHead = new Block(head.col, head.row + 1);
-	} else if (this.direction === "left") {
-		newHead = new Block(head.col - 1, head.row);
-	} else if (this.direction === "up") {
-		newHead = new Block(head.col, head.row - 1);
-	}
-
-	if (this.checkCollision(newHead)) {
-		gameOver();
-		return;
-	}
-
-	this.segments.unshift(newHead);
-
-	if (newHead.equal(apple.position)) {
-		score++;
-		apple.move();
-	} else {
-		this.segments.pop();
-	}
-};
-
-// Check if the snake collides with a wall or itself
-Snake.prototype.checkCollision = function(head) {
-	var leftCollision = (head.col === 0);
-	var topCollision = (head.row === 0);
-	var rightCollision = (head.col === widthInBlocks - 1);
-	var bottomCollision = (head.row === heightInBlocks - 1);
-
-	var wallCollision = leftCollision || topCollision || rightCollision || bottomCollision;
-
-	var selfCollision = false;
-
-	for (var i = 0; i < this.segments.length; i++) {
-		if (head.equal(this.segments[i])) {
-			selfCollision = true;
-		}
-	}
-
-	return wallCollision || selfCollision;
-};
-
-// Define the Apple constructor
+// Define the Apple object
 function Apple() {
-	this.position = new Block(10, 10);
-}
+  this.size = 20;
+  this.x = Math.floor(Math.random() * (canvas.width - this.size));
+  this.y = Math.floor(Math.random() * (canvas.height - this.size));
 
-// Draw the apple on the canvas
-Apple.prototype.draw = function() {
-	this.position.drawCircle("limegreen");
-};
+  // Define the draw method
+  this.draw = function () {
+    ctx.fillStyle = "#f00";
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  };
 
-// Move the apple to a random position
-Apple.prototype.move = function() {
+  // Define the move method
+  this.move = function () {
     this.x = Math.floor(Math.random() * (canvas.width - this.size));
     this.y = Math.floor(Math.random() * (canvas.height - this.size));
   };
-
-// Add event listeners for keyboard and touch controls
-document.addEventListener("keydown", function(event) {
-	switch (event.code) {
-		case "ArrowLeft":
-			if (snake.direction !== "right") {
-				snake.nextDirection = "left";
-			}
-			break;
-		case "ArrowUp":
-			if (snake.direction !== "down") {
-				snake.nextDirection = "up";
-			}
-			break;
-		case "ArrowRight":
-			if (snake.direction !== "left") {
-				snake.nextDirection = "right";
-			}
-			break;
-		case "ArrowDown":
-			if (snake.direction !== "up") {
-				snake.nextDirection = "down";
-			}
-			break;
-		default:
-			break;
-	}
-});
-
-var startX, startY, endX, endY;
-
-document.addEventListener("touchstart", function(event) {
-	startX = event.touches[0].pageX;
-	startY = event.touches[0].pageY;
-});
-
-document.addEventListener("touchmove", function(event) {
-	endX = event.touches[0].pageX;
-	endY = event.touches[0].pageY;
-
-	if (Math.abs(startX - endX) > Math.abs(startY - endY)) {
-		if (startX > endX && snake.direction !== "right") {
-			snake.nextDirection = "left";
-		} else if (startX < endX && snake.direction !== "left") {
-			snake.nextDirection = "right";
-		}
-	} else {
-		if (startY > endY && snake.direction !== "down") {
-			snake.nextDirection = "up";
-		} else if (startY < endY && snake.direction !== "up") {
-			snake.nextDirection = "down";
-		}
-	}
-});
-
-// Set up the game variables
-var snake = new Snake();
-var apple = new Apple();
-var score = 0;
-
-// Define the game loop function
-function gameLoop() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawScore();
-	snake.move();
-	snake.draw();
-	apple.draw();
-	drawBorder();
-
-	setTimeout(function() {
-		requestAnimationFrame(gameLoop);
-	}, 1000 / 10);
 }
 
-// Start the game loop
-gameLoop();
+// Add event listeners for keyboard and touch controls
+var isPaused = true;
+document.addEventListener("keydown", function (event) {
+  if (isPaused) {
+    isPaused = false;
+    gameLoop();
+  }
+  switch (event.code) {
+    case "ArrowLeft":
+      if (snake.direction !== "right") {
+        snake.nextDirection = "left";
+      }
+      break;
+    case "ArrowUp":
+      if (snake.direction !== "down") {
+        snake.nextDirection = "up";
+      }
+      break;
+    case "ArrowRight":
+      if (snake.direction !== "left") {
+        snake.nextDirection = "right";
+      }
+      break;
+    case "ArrowDown":
+      if (snake.direction !== "up") {
+        snake.nextDirection = "down";
+      }
+      break;
+    default:
+      break;
+  }
+});
+
+var touchStartX, touchStartY;
+var touchEndX, touchEndY;
+
+document.addEventListener(
+  "touchstart",
+  function (event) {
+    if (isPaused) {
+      isPaused = false;
+      gameLoop();
+    }
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
+  },
+  false
+);
+
+document.addEventListener(
+  "touchend",
+  function (event) {
+    touchEndX = event.changedTouches[0].clientX;
+    touchEndY = event.changedTouches[0].clientY;
+
+    var xDiff = touchEndX - touchStartX;
+    var yDiff = touchEndY - touchStartY;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+      if (xDiff > 0 && snake.direction !== "left") {
+        snake.nextDirection = "right";
+      } else if (snake.direction !== "right") {
+        snake.nextDirection = "left";
+      }
+    } else {
+      if (yDiff > 0 && snake.direction !== "up") {
+        snake.nextDirection = "down";
+      } else if (snake.direction !== "down") {
+        snake.nextDirection = "up";
+      }
+    }
+  },
+  false
+);
+
+// Initialize the Snake and Apple objects
+var snake = new Snake();
+var apple = new Apple();
+
+// Define the game loop
+function gameLoop() {
+  // Move the snake
+  snake.move();
+
+  // Check for collision with the border or the snake's own body
+  if (snake.checkCollision()) {
+    alert("Game Over!");
+    location.reload();
+  }
+
+  // Check for collision with the apple
+  if (snake.x === apple.x && snake.y === apple.y) {
+    snake.ateApple = true;
+    apple.move();
+  }
+
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Draw the snake and the apple
+  snake.draw();
+  apple.draw();
+
+  // Set the game speed and call the game loop recursively
+  setTimeout(gameLoop, 100);
+}
